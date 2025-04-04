42bbd54db4fee5c455995f6eee2e81ebbda372b9
Ritesh Yadav <ritesh.yadav@allen.in>
2025-04-04 13:19:17 +0530

    user name is not unique

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    username: { type: DataTypes.STRING, unique: true, allowNull: false },
+    username: { type: DataTypes.STRING, allowNull: false },
     email:    { type: DataTypes.STRING, unique: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false }
   });
